b120c44a98ebafda3f51825792284970c09eb7f7
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-04-21 21:18:51 +0700

    array helper - filter

---
+++ array-helper/filter.js
@@ -0,0 +1,28 @@
+var products = [
+  { name: 'jeruk', type: 'buah-buahan'},
+  { name: 'kangkung', type: 'sayuran'},
+  { name: 'mangga', type: 'buah-buahan'},
+  { name: 'buncis', type: 'sayuran'}
+];
+var filteredProducts = [];
+
+// Cara lama
+for (var i = 0; i < products.length; i++) {
+  if (products[i].type === 'buah-buahan') {
+    filteredProducts.push(products[i]);
+  }
+}
+
+console.log(filteredProducts); //hasilnya kangkung & buncis
+
+// Cara ES6
+products.filter(function(product) {
+  return product.type === 'buah-buahan';
+});
+
+// bisa disimpan di dalam variable
+var newProducts = products.filter(function(product) {
+  return product.type === 'buah-buahan';
+});
+
+console.log(newProducts); //hasilnya jeruk & mangga
