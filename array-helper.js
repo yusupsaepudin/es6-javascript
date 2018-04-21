163df95c509cac28235318b2c70c3652487346cb
Yusup Saepudin <yusupsaepudin19@gmail.com>
2018-04-21 09:35:15 +0700

    array helper - forEach

---
+++ array-helper.js
@@ -0,0 +1,37 @@
+/*=====================
+ES6 - forEach
+fungsi forEach lebih mudah pengguannya dibandingkan for loop biasa
+======================*/
+
+/* == DASAR ==*/
+var colors = ['red', 'green', 'blue'];
+
+for (var i = 0; i < colors.length; i++ ) {
+  console.log(colors[i]);
+}
+// ES6 Feature
+colors.forEach(function(color) {
+  console.log(color);
+});
+
+/* == FITUR LANJUTAN ==*/
+// Create an array of numbers
+var numbers = [1 , 2, 3, 4, 5];
+
+// Create a variable to hold the sum
+var sum = 0;
+
+function adder(number) {
+  sum += number;
+}
+
+// // Loop over the array, incrementing the sum variable
+// numbers.forEach(function(number) {
+//   sum += number;
+// });
+
+// OR
+numbers.forEach(adder);
+
+// // Print the sum variable
+console.log(sum);
